e3db9704339c75ca6c0035474ea984e6aebe071e
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-08-24 18:30:52 +0300

    slider done

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// $(document).ready(function(){
-//     $('.single-item').slick({
-//         // respondTo:$slides,
-//     });
-// });
+$(document).ready(function(){
+    $('.single-item').slick({
+        prevArrow: '<img class="arrow_right" src="img/arrow_right.png"/>',
+        nextArrow: '<img class="arrow_left" src="img/arrow_left.png"/>',
+
+        // respondTo:$slides,
+    });
+});
