a0424f472546d3496c9978fa8c01d51916ad37e3
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-08-27 18:19:44 +0300

    script

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
     $('.block-reviews_single-item').slick({
-        prevArrow: '<img class="img__arrow_right" src="../img/img__arrow_right.png"/>',
-        nextArrow: '<img class="img__arrow_left" src="../img/img__arrow_left.png"/>',
+        prevArrow: '<img class="img__arrow_right" src="img/img__arrow_right.png"/>',
+        nextArrow: '<img class="img__arrow_left" src="img/img__arrow_left.png"/>',
     });
 
     $('.menu__button').click(function (e) {
