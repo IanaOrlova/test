9dc94c945d50dbba5ea391a281818a7e05dc1d08
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-08-28 14:12:52 +0300

    slick

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
 
+    var $status = $('.pagingInfo');
+    var $slickElement = $('.slideshow');
+
+    $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $status.text(i + ' OF ' + slick.slideCount);
+    });
+
     $('.block-reviews_single-item').slick({
         prevArrow: '<div class="img__arrow_right"><a><img src="img/img__arrow_right.png"/></a></div>',
         nextArrow: '<div class="img__arrow_left"><a><img  src="img/img__arrow_left.png"/></a></div>',
+        // dots: true,
+
+
     });
 
     $('.menu__button').click(function (e) {
